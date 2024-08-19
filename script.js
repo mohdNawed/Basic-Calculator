50b1ae11a2a5656e23c3de3cd95cd9a32ed13fb6
mohdNawed <md.23scse2030244@galgotiasuniversity.ac.in>
2024-08-19 19:48:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+let input = document.getElementById('inputBox');
+let buttons = document.querySelectorAll('button');
+
+let string = "";
+let arr = Array.from(buttons);
+
+arr.forEach(button => {
+    button.addEventListener('click', (e) => {
+        let buttonText = e.target.innerHTML;
+
+        if (buttonText == '=') {
+            try {
+                string = eval(string);
+                input.value = string;
+            } catch (error) {
+                input.value = "Error";
+            }
+        } else if (buttonText == 'AC') {
+            string = "";
+            input.value = string;
+        } else if (buttonText == 'DEL') {
+            string = string.substring(0, string.length - 1);
+            input.value = string;
+        } else {
+            string += buttonText;
+            input.value = string;
+        }
+    });
+});
